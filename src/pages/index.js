ca73e1488fd142796e3902f04877d916fc43f09e
dDaiver <bobkovmv@gmail.com>
2024-03-17 12:14:57 +0300

    yandex metric

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,13 @@ import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 import { Link } from 'gatsby'
 import { graphql, useStaticQuery } from 'gatsby'
-import { Script } from 'gatsby'
+import YandexMetric from '../components/yandexmetric'
 import Helmet from "react-helmet"
 
 
 const IndexPage = ()=> {
   return (
+      
       <Layout pageTitle="Главная">
         <p>I'm making this by following the Gatsby Tutorial.</p>
         <StaticImage
@@ -26,9 +27,7 @@ const IndexPage = ()=> {
                     <th></th>
                 </tr>
             </table>
-            <Helmet>
-              
-            </Helmet>
+            <YandexMetric />
 
       </Layout>
   )
